e756b0f18862b796594520d09270dfe90de76a1c
Victor Litvinenko <v.litvinenko@intelico.su>
2020-10-01 16:01:03 +0300

    fix: replace exitcode

--- task1/validate.js
+++ task1/validate.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 exports.validate = function (action, shift, inputFilePath, outputFilePath) {
   if (!action || !shift) {
     console.error('Action and shift are required options!');
-    process.exit(9);
+    process.exit(1);
   }
   if (shift < 0) {
     console.error('Shift must be positive!');
-    process.exit(9);
+    process.exit(1);
   }
   if (inputFilePath && (!fs.existsSync(inputFilePath) || !fs.lstatSync(inputFilePath).isFile())) {
     console.error('Input file "' + inputFilePath + '" does not exist!');
-    process.exit(9);
+    process.exit(1);
   }
   if (outputFilePath && (!fs.existsSync(outputFilePath) || !fs.lstatSync(outputFilePath).isFile())) {
     console.error('Output file "' + outputFilePath + '" does not exist!');
-    process.exit(9);
+    process.exit(1);
   }
 }
