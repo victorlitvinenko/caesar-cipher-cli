312cdd5aec685638f31e78a72fa6a7330a91899e
Victor Litvinenko <v.litvinenko@intelico.su>
2020-10-01 15:35:06 +0300

    fix: improve validations

--- task1/validate.js
+++ task1/validate.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 
 exports.validate = function (action, shift, inputFilePath, outputFilePath) {
-  if (inputFilePath && !fs.existsSync(inputFilePath)) {
-    console.error('Input file path is invalid!');
-    process.exit(9);
-  }
   if (!action || !shift) {
     console.error('Action and shift are required options!');
     process.exit(9);
   }
   if (shift < 0) {
     console.error('Shift must be positive!');
+    process.exit(9);
+  }
+  if (inputFilePath && (!fs.existsSync(inputFilePath) || !fs.lstatSync(inputFilePath).isFile())) {
+    console.error('Input file "' + inputFilePath + '" does not exist!');
+    process.exit(9);
+  }
+  if (outputFilePath && (!fs.existsSync(outputFilePath) || !fs.lstatSync(outputFilePath).isFile())) {
+    console.error('Output file "' + outputFilePath + '" does not exist!');
     process.exit(9);
   }
 }
